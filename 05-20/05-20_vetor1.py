bceccbfdb1db573e65fb77903653e96fa119f225
unknown <djhonathanhenriquepaiva@gmail.com>
2021-05-27 20:57:58 -0300

    atualizando - 05-27

--- 05-20/05-20_vetor1.py
+++ 05-20/05-20_vetor1.py
@@ -36,37 +36,3 @@ v2 = [0]*10 #mas tem essa opcao
 print(len(v2))
 print(v2)
 '''
-
-
-'''
-exercicio: 
-preencher um vetor com 100 numeros sorteados de 10 a 99 [10,99],
-depois pergunta uma busca nesse vetor,
-depois mostra se essa busca existe.
-
-comandos e funções úteis:
-
-- from random import randint
-
-- randint(0, 2)
-
-- for i in range(1, 11, 1):
-
-- v2 = [0]*10
-'''
-
-from random import randint  #pra usar a biblioteca de valores randons/sorteados
-
-v = [0]*100
-for i in range(0, 100, 1):
-    v[i] = randint(10,99)
-print(v)
-
-busca = int(input("Qual a chave de busca?: "))
-qtde = 0
-
-for i in range(0,100,1):
-    if busca == v[i]:
-        qtde = qtde + 1
-        print(f'Encontrei o valor {busca} na posicao {i}')
-print(f'Achei <{qtde}> ocorrências de {busca}')
