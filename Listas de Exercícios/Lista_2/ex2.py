842cf0213893e795a3fae0cc9ca60c80ef3a3792
unknown <djhonathanhenriquepaiva@gmail.com>
2021-07-01 18:12:44 -0300

    ...

--- Listas de Exercícios/Lista_2/ex2.py
+++ Listas de Exercícios/Lista_2/ex2.py
@@ -12,4 +12,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 mA = np.array([[5,4.5,7,5.2,6.1], [2.1,6.5,8,7,6.7], [8.6,7,9.1,8.7,9.3]])
-print(f'\n', mA, '\n')
+vetSave = [0]*3 # cria um vetor vazio de 3 posições
+print(f'matriz A:\n', mA, '\n')
+
+for i in range(len(mA)):   # retorna o total de linhas
+   for j in range(0, 5, 1):
+      print(f'elemento [{i} , {j}]: {mA[i][j]}') #print dos elementos
+   print()
+
+for i in range(len(mA)):
+   for j in range(len(mA[i])):  #len() ou range(0,5,1) 
+      vetSave[i] = sum(mA[i]) #vetor que armazena a soma das linhas advém da função sum(), que no caso está armazenando a soma dos elementos de cada linha da matriz mA.
+    
+for n in range(3):
+   print(f'soma da linha {n}: {vetSave[n]}')
